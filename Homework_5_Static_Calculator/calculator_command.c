5912c1269ba3a552aa02ec1ad56d1ae305c8635d
jet006 <46136069+jet006@users.noreply.github.com>
2022-02-12 18:24:26 +0700

    Add files via upload

---
+++ Homework_5_Static_Calculator/calculator_command.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int summ(int a,int b){
+	int c;
+	c = a + b;
+	return c;
+}
+int substr(int a,int b){
+    int c;
+    if (a>=b){
+        c = a - b;
+        return c;
+    }
+    else {
+        c = b - a;
+        return c;
+    }
+}
+int mult(int a,int b){
+    int c;
+	c = a * b;
+	return c;
+}
+int divis(int a,int b){
+    int c;
+	c = a / b;
+	return c;
+}
---
+++ Homework_5_Static_Calculator/calculator_command.h
@@ -0,0 +1,9 @@
+#ifndef CALCULATOR_COMMAND_H_INCLUDED
+#define CALCULATOR_COMMAND_H_INCLUDED
+int summ(int, int);
+int subtr(int, int);
+int mult(int, int);
+int divis(int, int);
+
+
+#endif // CALCULATOR_COMMAND_H_INCLUDED
